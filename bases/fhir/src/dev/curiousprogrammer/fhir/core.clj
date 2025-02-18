ccd6361432adfa355f2d97de96b6f0cabd9e7339
Clarice Bouwer <cbillowes@gmail.com>
2025-02-18 20:38:11 +0400

    Create a super simple FHIR service

--- bases/fhir/src/dev/curiousprogrammer/fhir/core.clj
+++ bases/fhir/src/dev/curiousprogrammer/fhir/core.clj
@@ -1,1 +1,20 @@
-(ns dev.curiousprogrammer.fhir.core)
+(ns dev.curiousprogrammer.fhir.core
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
+
+
+
+(def routes
+  (route/expand-routes
+   #{["health" :get (fn [_] {:status 200 :body "OK"}) :route-name :health]}))
+
+
+(def service
+  {:env :prod
+   ::http/routes routes
+   ::http/type :jetty
+   ::http/port 4334})
+
+
+(defn start []
+  (http/start (http/create-server service)))
