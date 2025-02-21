2cc21e667f0c7bbbe20d022afb5fb8657585cfb9
Clarice Bouwer <cbillowes@gmail.com>
2025-02-21 14:05:27 +0400

    Remove the service base

--- bases/service/test/dev/curiousprogrammer/service/core_test.clj
+++
@@ -1,6 +0,0 @@
-(ns dev.curiousprogrammer.service.core-test
-  (:require [clojure.test :as test :refer :all]
-            [dev.curiousprogrammer.service.core :as core]))
-
-(deftest dummy-test
-  (is (= 1 1)))
