f23fa598abd3896d8da1f428f3fb693acfcc12c5
Clarice Bouwer <cbillowes@gmail.com>
2025-02-23 12:36:37 +0400

    Redesign the home page

--- bases/webapp/src/dev/curiousprogrammer/app/pages/home.cljs
+++ bases/webapp/src/dev/curiousprogrammer/app/pages/home.cljs
@@ -1,9 +1,8 @@
-(ns dev.curiousprogrammer.app.pages.home
-  (:require [dev.curiousprogrammer.app.router :refer [url-for]]))
+(ns dev.curiousprogrammer.app.pages.home)
+
 
 (defn page []
-  [:div.text-white.bg-red-400
-   [:h1 "Welcome to the Curious Programmer"]
-   [:p "This is a simple web application to demonstrate the use of Clojure and ClojureScript in a full-stack application."]
-   [:a {:href (url-for :home)} "Go home"][:br]
-   [:a {:href (url-for :search)} "Search for something"]])
+  [:div {:class "my-8 max-w-4xl w-2/3 mx-auto text-center text-white leading-relaxed"}
+   [:h1.font-bold.my-2.text-4xl "Welcome to the FHIR playground"]
+   [:p.mb-2 "FHIR is a standard for exchanging healthcare information electronically. It is a set of rules that allow healthcare systems to work together and share information. It stands for Fast Healthcare Interoperability Resources."]
+   [:p "This application uses the " [:a.underline.text-yellow-400.hover:text-gray-400 {:href "https://hapi.fhir.org/baseR4" :target "_blank"} "HAPI API"] " to fetch data."]])
