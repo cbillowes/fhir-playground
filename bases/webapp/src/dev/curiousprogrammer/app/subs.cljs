617c53064482e481c5af63721143726be5235e9f
Clarice Bouwer <cbillowes@gmail.com>
2025-02-23 12:09:19 +0400

    Set active page sub to derive page name from URL pathname

--- bases/webapp/src/dev/curiousprogrammer/app/subs.cljs
+++ bases/webapp/src/dev/curiousprogrammer/app/subs.cljs
@@ -1,9 +1,15 @@
 (ns dev.curiousprogrammer.app.subs
-  (:require [re-frame.core :as rf]))
+  (:require [clojure.string :as str]
+            [re-frame.core :as rf]))
 
 
 (rf/reg-sub
  :active-page
  (fn [db _]
-   (or (:active-page db) :home)))
+   (let [pathname (.-pathname js/window.location)
+         page-name (-> (str/split pathname #"/")
+                       (second)
+                       (keyword)
+                       (or :home))]
+     (or (:active-page db) page-name))))
 
